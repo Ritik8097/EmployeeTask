021e2a9d0736439c1846a1069fd47f8fabf28c6e
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-04-23 14:37:20 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     setError('');
     
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post('https://employeetaskbackend.onrender.com/api/auth/login', {
         email,
         password
       });
